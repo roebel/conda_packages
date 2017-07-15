33ed441e7e5bd120ead937e5b27751e0a4bd9fa7
Axel Roebel <Axel.Roebel@ircam.fr>
2017-07-15 14:04:38 +0200

    removed as_pysrc from master

--- as_pysrc/README.txt
+++
@@ -1,2 +0,0 @@
-This package is ircam internal only for binary distribution of analysis/synthesis tams signal processing envinrment.
-You cannot use this if you don't have access to the as_pysrc repos on forge.ircam.fr.
--- as_pysrc/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git clone git+ssh://git@git.forge.ircam.fr/as_pysrc.git
-cd as_pysrc
-make install
-rsync -a  --exclude "adapt/" --exclude "transposition_script/" --exclude "shell_scripts/" --include '*/' --include '*.py' --exclude '*' * $PREFIX/lib/python3.6/site-packages/
-cd ..
